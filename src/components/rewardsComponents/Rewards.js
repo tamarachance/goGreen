3dafdcf677dd3dfd6e56fd76948cc623013a41a6
Tamara Chance <tamara.n.chance@gmail.com>
2022-01-17 16:34:58 -0600

    init rewards screen

---
+++ src/components/rewardsComponents/Rewards.js
@@ -0,0 +1,16 @@
+import React from "react";
+import HamNav from "../homeComponents/HamNav";
+import Menubar from "../homeComponents/Menubar";
+import Logo from "../landingPageComponents/Logo";
+
+const Rewards = () => {
+    return ( 
+        <div className="rewards">
+            <HamNav />
+            <Logo />
+            <Menubar />
+        </div>
+     );
+}
+ 
+export default Rewards;
