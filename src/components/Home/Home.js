aa808c15516226c5dd65393c12d43072b34d3482
Tamara Chance <tamara.n.chance@gmail.com>
2022-02-02 17:20:21 -0600

    added point states

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,23 +6,22 @@ import NavHeader from '../NavHeader/NavHeader';
 import NavSide from '../NavSide/NavSide';
 
 const Home = (props) => {
+        
     return (
-                  
-
           <div className="home">
             <NavHeader />
             <NavSide isLoggedIn={props.isLoggedIn} menuClick={props.menuClick} setActive={props.setActive} isActive={props.isActive}/>
             <Logo menuClick={props.menuClick}/>
             <div className="points-container">
               <h2>Available Points:</h2>
-              <h1>USER.POINTS WOULD GO HERE</h1>
+              <h1>{props.availablePoints}</h1>
             </div>
             <div 
               className="points-container"
               style={{top: 352}}
             >
               <h2>Total Points Earned:</h2>
-              <h1>USER.POINTS ADDED UP WOULD GO HERE</h1>
+              <h1>{props.totalPointsEarned}</h1>
 
             </div>
             <Footer />
