dee902edb91d056f80122ec612d9949773403407
Tamara Chance <tamara.n.chance@gmail.com>
2022-02-02 11:45:48 -0600

    completed client side upload features

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './upload.css';
 import Logo from '../Logo/Logo';
 import Footer from '../Footer/Footer';
@@ -6,51 +6,93 @@ import Dropzone from "react-dropzone-uploader";
 import 'react-dropzone-uploader/dist/styles.css';
 import NavHeader from '../NavHeader/NavHeader';
 import NavSide from '../NavSide/NavSide';
+import swal from 'sweetalert';
+import axios from 'axios';
+import {useFormik} from 'formik';
+import { Navigate, useNavigate } from 'react-router';
+
 
 function Upload({ fetchUploads, menuClick, isLoggedIn, setActive, isActive }) {
-    const getUploadParams = () => console.log('something uploaded goes here')
-//   const getUploadParams = ({ file }) => {
-//     const body = new FormData()
-//     body.append('image', file)
-//     return {
-//       url: 'http://localhost:8000/api/uploads',
-//       body
-//     };
-//   }
-    const handleSubmit=()=> console.log('you uploaded a photo')
-//   const handleSubmit = (files, allFiles) => {
-//     allFiles.forEach(f => f.remove())
-//     fetchUploads();
-//   }
+    const navigate = useNavigate()
+    const [url, setUrl] = useState("")
 
+    const initialValues = {
+        actionType:"250",
+        description:" "
+    }
+
+    const onSubmit = (values) => {
+        const todayDate = new Date();
+        const userID = localStorage.getItem('id')
+        const bodyObject = {
+            user_id: userID,
+            image_Url: url,
+            actionValue: values.actionType,
+            description: values.description,
+            createdAt: todayDate
+        }
+        // console.log(bodyObject)
+        axios.post("http://localhost:4000/uploads", bodyObject)
+            .then(() => {
+                swal({
+                    title: 'Your rewards are on the way!',
+                    text: 'Please allow up to 24hrs for your points to post.',
+                    icon: "success"
+                })
+        }).then(navigate('/home'))       
+    }
+
+    const formik = useFormik({
+        initialValues,
+        onSubmit,
+        
+    })
+    
+    const getUploadParams = async ({ file }) => {
+        const body = new FormData()
+        body.append('image', file)
+        const result = await axios.post('http://localhost:4000/images', body, { headers: {'Content-Type': 'multipart/form-data'}})
+        setUrl(result.data.imagePath)        
+        
+    }
+   
   return (
     <div className='upload'>
         <NavHeader />
         <NavSide isLoggedIn={isLoggedIn} menuClick={menuClick} setActive={setActive} isActive={isActive}/>
         <Logo menuClick={menuClick}/>
-        <div className='upload-container'>  
+        <form className='upload-container' onSubmit={formik.handleSubmit}>  
             <Dropzone
                 getUploadParams={getUploadParams}
-                onSubmit={handleSubmit}
                 accept="image/*"
                 maxFiles={1}
                 multiple={false}
                 styles={{
-                dropzone: { width: 254, maxHeight: 250}
+                dropzone: { width: 254, maxHeight: 250},
+            
                 }}
             />
             <div className='button-container'>
-                <input type="radio" /> Reduced your consumption/emission
+                <label htmlFor="actions">Which sustainable action did you take today?</label>
+                <select name="actionType" id="actions" onChange={formik.handleChange} value={formik.values.actionType} >
+                    <option value="2500">Reduced emmisions/consumptions</option>
+                    <option value="2000">Composted</option>
+                    <option value="1000">Upcycled into something new</option>
+                    <option value="500">Reused/Donated for reuse</option>
+                    <option value="250">Recycled</option>
+                </select>
                 <br />
-                <input type="radio" /> Reused an item or donated for reuse
-                <br />
-                <input type="radio" /> Recycled <br />
-                <br />
-                Describe your photo:<input type="text" size="lg" />
+                <label>Describe your photo:<input 
+                    type="text" 
+                    size="lg" 
+                    name='description' 
+                    onChange={formik.handleChange} 
+                    value={formik.values.description} 
+                /></label>
             </div>
             <br />
-            <button>Submit</button>
-        </div>
+            <button type="submit" disabled={!formik.isValid}>Submit</button>
+        </form>
         <Footer />
     </div>
   );
